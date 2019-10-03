aeb600857435df2d5d09dcce1bba32c6f6c4701d
abdonaser10 <40724937+abdonaser10@users.noreply.github.com>
2019-10-03 16:51:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,46 @@ var adminprefix = '%'
 
 
 
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("612623349180399626");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("629295968386416640");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("612623349180399626");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
 
   var prefix = "%";
 
